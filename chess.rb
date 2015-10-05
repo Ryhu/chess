fe45b783e18a114c32c65a069df50bd4e7eb65ee
justinandryan <justinandryan@appacademy.io>
2015-10-05 16:16:49 -0400

    Add basic cursor functionality

--- chess.rb
+++ chess.rb
@@ -1,9 +1,13 @@
 require 'colorize'
+require "io/console"
 
 class ChessError < StandardError
+
 end
 
+
 class Board
+  attr_reader :grid
   def initialize
     @grid = Array.new(8) {Array.new(8)}
     populate_grid
@@ -30,14 +34,17 @@ class Board
   def populate_grid
     start_rows = [0, 1, 6, 7]
 
-    grid.each_index do |row|
-      if start_rows.include?(row)
-        row.each do |col|
+    @grid.each_index do |row|
+      @grid[row].each_index do |col|
+        if start_rows.include?(row)
           self[[row,col]] = Piece.new([row,col])
+        else
+          self[[row, col]] = "   "
         end
       end
     end
   end
+
 end
 
 class Display
@@ -54,8 +61,8 @@ class Display
   MOVES = {
     left:  [0, -1],
     right: [0, 1],
-    up:    [1, 0],
-    down:  [-1, 0]
+    up:    [-1, 0],
+    down:  [1, 0]
   }
 
   def initialize(board)
@@ -68,11 +75,28 @@ class Display
     handle_key(key)
   end
 
+  def read_char
+    STDIN.echo = false
+    STDIN.raw!
+
+    input = STDIN.getc.chr
+    if input == "\e" then
+      input << STDIN.read_nonblock(3) rescue nil
+      input << STDIN.read_nonblock(2) rescue nil
+    end
+  ensure
+    STDIN.echo = true
+    STDIN.cooked!
+
+    return input
+  end
+
   def handle_key(key)
     case key
     when :space
-      # select pos
+      nil
     when :backspace
+      nil
       # deselect pos
     when :left, :right, :up, :down
       update_pos(MOVES[key])
@@ -83,13 +107,13 @@ class Display
 
   def update_pos(coords)
     @cursor_pos[0] += coords[0]
-    @cursos_pos[1] += coords[1]
+    @cursor_pos[1] += coords[1]
   end
 
-  def colors_for(x,y)
-    if [x, y] = @cursor_pos
+  def colors_for(x, y)
+    if [x, y] == @cursor_pos
       bg = :yellow
-    elsif (x + y) = odd
+    elsif (x + y).odd?
       bg = :black
     else
       bg = :white
@@ -97,11 +121,43 @@ class Display
     { background: bg }
   end
 
+  def render
+    system("clear")
+    build_grid.each {|row| puts row.join}
+  end
+
+  def build_grid
+    @board.grid.map.with_index do |row,i|
+      build_row(row,i)
+    end
+  end
+
+  def build_row (row,row_index)
+    row.map.with_index do |col, col_index|
+      color_options = colors_for(row_index, col_index)
+      col.to_s.colorize(color_options)
+    end
+  end
 
 end
 
-class Piece()
+class Piece
   attr_reader :pos
+
   def initialize(pos)
     @pos = pos
+  end
+
+  def to_s
+    " P ".colorize(:red)
+  end
+
 end
+
+board = Board.new
+disp = Display.new(board)
+3.times do
+  disp.render
+  disp.get_input
+end
+disp.render
